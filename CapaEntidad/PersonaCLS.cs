c58f8108bfb4c11b4f5ba68343ab138f0e7a6dda
Rubi Veloz Jimenez <147438464+rubivj13@users.noreply.github.com>
2026-02-10 13:02:38 -0600

    Seguimieto Programa !0/02/1026

---
+++ CapaEntidad/PersonaCLS.cs
@@ -0,0 +1,21 @@
+﻿namespace CapaEntidad
+{
+    public class PersonaCLS
+    {
+        //Listar
+        public int iidpersona { get; set; }
+        public string nombrecompleto { get; set; }
+        public string correo { get; set; }
+        public string fechanacimientocadena { get; set; }
+
+
+        //Recuperar datos para editar
+
+        public string nombre { get; set; }
+        public string appaterno { get; set; }
+        public string apmaterno { get; set; }
+        public DateTime? fechanacimiento { get; set; }
+        public int iidsexo { get; set; }
+
+    }
+}
---
+++ Web.Api/Controllers/PersonaController.cs
@@ -0,0 +1,69 @@
+﻿using CapaEntidad;
+using Microsoft.AspNetCore.Mvc;
+using Web.Api.Models;
+
+namespace Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet("{nombrecompleto}")]
+        public List<PersonaCLS> listarPersona(string nombrecompleto)
+        {
+            List<PersonaCLS> lista = new List<PersonaCLS>();
+            using (DbAc5136DbveterinariaContext bd = new DbAc5136DbveterinariaContext())
+            {
+                lista = (from persona in bd.Personas
+                         where persona.Bhabilitado == 1
+                         && (persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno).Contains(nombrecompleto)
+
+                         select new PersonaCLS
+                         {
+                             iidpersona = persona.Iidpersona,
+                             nombrecompleto = persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno,
+                             correo = persona.Correo,
+                             fechanacimientocadena = persona.Fechanacimiento == null ? " "
+                             : persona.Fechanacimiento.Value.ToString("dd/MM/yyyy"),
+
+                         }).ToList();
+            }
+            return lista;
+        }
+
+        //Recuperar registro por id
+        [HttpGet("recuperarPersona/{id}")]
+        public PersonaCLS recuperarPersona(int id)
+        {
+            PersonaCLS oPersonaCLS = new PersonaCLS();
+
+            //List<PersonaCLS> lista = new List<PersonaCLS>();
+
+            try
+            {
+                using (DbAc5136DbveterinariaContext bd = new DbAc5136DbveterinariaContext())
+                {
+
+                    oPersonaCLS = (from persona in bd.Personas
+                                   where persona.Bhabilitado == 1
+                                   && persona.Iidpersona == id
+                                   select new PersonaCLS
+                                   {
+                                       iidpersona = persona.Iidpersona,
+                                       nombrecompleto = persona.Nombre + " " + persona.Appaterno + " " + " " + persona.Apmaterno,
+                                       correo = persona.Correo,
+                                       fechanacimientocadena = persona.Fechanacimiento == null ? " "
+                                       : persona.Fechanacimiento.Value.ToString("dd/MM/yyyy"),
+                                   }).First();
+                }
+                return oPersonaCLS;
+            }
+            catch (Exception ex)
+            {
+                return oPersonaCLS;
+            }
+
+        }
+
+    }
+}
